223758b2bbe0655a80cafd1f208be181b7e5a2ba
DaveCalaway <davide.gariselli@gmail.com>
2016-12-10 18:17:23 +0100

    update

--- Arduino/aqp_sensor_ada.ino
+++ Arduino/aqp_sensor_ada.ino
@@ -76,17 +76,28 @@
 */
 
 // ---- WiFi connection ----------
-#define WLAN_SSID       "FreeLepida_Fiorano"
-#define WLAN_PASS       ""
-WiFiClient client;
+#define WLAN_SSID       "xxx"
+#define WLAN_PASS       "xxx"
 
 // ----- MQTT connection ---------
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "...your AIO username (see https://accounts.adafruit.com)..."
-#define AIO_KEY         "...your AIO key..."
+#define AIO_USERNAME    "xxx"
+#define AIO_KEY         "xxx"
+WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 //----------------------------------
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'dht11' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish dht = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/dht");
+
+// Setup a feed called 'onoff' for subscribing to changes.
+// Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff");
+
+/************************************************************/
 
 #ifdef oled
 // Include custom images if you want
@@ -116,17 +127,6 @@ byte cnt_heart = CICLI_HEART;
   String msg_type = "";
   String msg_data = "";
 */
-
-/****************************** Feeds ***************************************/
-
-// Setup a feed called 'dht11' for publishing.
-// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish dht = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/dht");
-
-// Setup a feed called 'onoff' for subscribing to changes.
-// Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff");
-
-/************************************************************/
 
 void MQTT_connect() {
   int8_t ret;
@@ -252,6 +252,7 @@ void setup()
 
 /* ---------- MAIN LOOP ---------- */
 void loop() {
+  Serial.println("lol");
   MQTT_connect();
 
   // this is our 'wait for incoming subscription packets' busy subloop
